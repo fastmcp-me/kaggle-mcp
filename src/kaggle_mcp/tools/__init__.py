c4b84bda47ffb9696beada091b49e19b2309d101
Steven Yu <2322740290@qq.com>
2025-04-06 17:39:44 -0400

    refactor: Restructure tool initialization to avoid circular imports

--- src/kaggle_mcp/tools/__init__.py
+++ src/kaggle_mcp/tools/__init__.py
@@ -1,8 +1,23 @@
-"""Tools package initialization."""
+"""Tools package initialization.
 
-from kaggle_mcp.tools.auth import *
-from kaggle_mcp.tools.competitions import *
-from kaggle_mcp.tools.datasets import *
-from kaggle_mcp.tools.kernels import *
-from kaggle_mcp.tools.models import *
-from kaggle_mcp.tools.config import *
+This module exports the API instance and tool initialization functions
+to avoid circular import issues.
+"""
+
+from kaggle_mcp.tools.auth import init_auth_tools, api, ensure_authenticated
+from kaggle_mcp.tools.competitions import init_competition_tools
+from kaggle_mcp.tools.datasets import init_dataset_tools
+from kaggle_mcp.tools.kernels import init_kernel_tools
+from kaggle_mcp.tools.models import init_model_tools
+from kaggle_mcp.tools.config import init_config_tools
+
+__all__ = [
+    'init_auth_tools',
+    'init_competition_tools',
+    'init_dataset_tools',
+    'init_kernel_tools',
+    'init_model_tools',
+    'init_config_tools',
+    'api',
+    'ensure_authenticated',
+]
